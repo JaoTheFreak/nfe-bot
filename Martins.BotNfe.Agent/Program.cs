777f080a3255f39a653da01e170cf8a29867207d
João Antonio Martins Filho <joaoantoniomartinsfilho@gmail.com>
2020-01-08 23:24:02 -0300

    Starting coding the bot

---
+++ Martins.BotNfe.Agent/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace Martins.BotNfe.Agent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var browserDriverPath = "/home/joaomartins/Downloads/";
+
+            var baseUrl = "https://www.fsist.com.br/";
+
+            var nfe = "32191205570714000825550010061807591137555748";
+
+            IWebDriver browser = new FirefoxDriver(browserDriverPath);
+
+            browser.Navigate().GoToUrl(baseUrl);
+
+            var nfeKeyField = browser.FindElement(By.Id("chave"));
+
+            nfeKeyField.SendKeys(nfe);
+
+            var nfeButtonQuery = browser.FindElement(By.Id("butconsulta"));
+
+            nfeButtonQuery.Click();
+
+            
+        }
+
+
+    }
+}
